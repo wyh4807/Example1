70ace64eb99ec1c8b5a9eee87baf59f3ed0c0034
woo <wyh4807@naver.com>
2018-11-24 14:30:29 +0900

    push
    jdbc 추가

--- src/main/java/com/Example1/config/H2Config.java
+++ src/main/java/com/Example1/config/H2Config.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class H2Config {
-	static final String h2WebConsoleUrl = "/WooH2/*";
+	static final String h2WebConsoleUrl = "/H2/*";
 
     @Bean
     ServletRegistrationBean<WebServlet> h2ServletRegistration() {
